2591cec809e2479148baef495577a37bd5e3ff1d
k0stro <pelek94@gmail.com>
2025-09-13 17:08:22 +0200

    feat: add predict endpoint to API; define input and output data schemas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.schemas import HousePriceInputData, HousePriceOutputData
+from app.model import predict
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/predict", response_model=HousePriceOutputData)
+def predict_price(input_data: HousePriceInputData):
+    pass
