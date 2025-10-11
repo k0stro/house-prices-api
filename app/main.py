c06e012e1bd1e24aab9193085bf9eb3ddd47c93e
k0stro <pelek94@gmail.com>
2025-10-11 17:45:18 +0200

    refactor: update predict endpoint to use dummy input example in request body

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from app.schemas import ProcessedHousePriceInputData, HousePriceOutputData
+from fastapi import FastAPI, Body
+from app.schemas import ProcessedHousePriceInputData, HousePriceOutputData, dummy_input
 from app.model import predict
 
 app = FastAPI()
@@ -10,7 +10,7 @@ def health():
 
 
 @app.post("/predict", response_model=HousePriceOutputData)
-def predict_price(input_data: ProcessedHousePriceInputData): #ProcessedHousePriceInputData to be swapped with RawProcessedHousePriceInputData for raw input after adding preprocessing step
+def predict_price(input_data: ProcessedHousePriceInputData = Body(..., example=dummy_input)): #ProcessedHousePriceInputData to be swapped with RawProcessedHousePriceInputData for raw input after adding preprocessing step
     features = list(input_data.model_dump().values())
     prediction = predict(features)
     return HousePriceOutputData(SalePrice=prediction)
