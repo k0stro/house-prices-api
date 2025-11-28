1bb559b0f71200e34d89a8d0f7c81f35d04a0ff8
k0stro <pelek94@gmail.com>
2025-11-28 21:20:57 +0100

    fix: update example input data in predict_price function to use sample_raw_data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Body
-from app.schemas import RawHousePriceInputData, HousePriceOutputData, dummy_input
+from app.schemas import RawHousePriceInputData, HousePriceOutputData, sample_raw_data
 from app.model import predict
 
 app = FastAPI()
@@ -10,7 +10,7 @@ def health():
 
 
 @app.post("/predict", response_model=HousePriceOutputData)
-def predict_price(input_data: RawHousePriceInputData = Body(..., example=dummy_input)):
+def predict_price(input_data: RawHousePriceInputData = Body(..., example=sample_raw_data)):
     features = list(input_data.model_dump().values())
     prediction = predict(features)
     return HousePriceOutputData(SalePrice=prediction)
