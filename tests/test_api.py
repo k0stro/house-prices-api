354279a5e0ab777f9b3d9a05173ced9ee6cd0c27
k0stro <pelek94@gmail.com>
2025-12-14 18:16:01 +0100

    test: update predict endpoint test to use sample_raw_data instead of dummy_input

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from app.schemas import dummy_input
+from app.schemas import sample_raw_data
 
 client = TestClient(app)
 
 def test_predict_endpoint():
-    response = client.post("/predict", json=dummy_input)
+    response = client.post("/predict", json=sample_raw_data)
     assert response.status_code == 200
     data = response.json()
     assert "SalePrice" in data
-    assert isinstance(data["SalePrice"], float) 
+    assert isinstance(data["SalePrice"], float)
