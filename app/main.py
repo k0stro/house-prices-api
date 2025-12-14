cd7e011d645992221b59bb755e5c0631ecf9da4d
k0stro <pelek94@gmail.com>
2025-12-14 18:15:54 +0100

    feat: integrate preprocessing in predict_price function using encoding map and scaler

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI, Body
-from app.schemas import RawHousePriceInputData, HousePriceOutputData, sample_raw_data
-from app.model import predict
+from app.schemas import RawHousePriceInputData, HousePriceOutputData, sample_raw_data, TRAIN_COLUMNS
+from app.model import predict, load_encoding_map, load_scaler
+from app.preprocessing import preprocess_data
 
 app = FastAPI()
+encoding_map = load_encoding_map()
+scaler = load_scaler()
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-
 @app.post("/predict", response_model=HousePriceOutputData)
 def predict_price(input_data: RawHousePriceInputData = Body(..., example=sample_raw_data)):
-    features = list(input_data.model_dump().values())
-    prediction = predict(features)
+    input_preprocess = preprocess_data(input_data, encoding_map=encoding_map, scaler=scaler, train_columns=TRAIN_COLUMNS)
+    prediction = predict(input_preprocess)
     return HousePriceOutputData(SalePrice=prediction)
